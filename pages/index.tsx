895e67fe400133d03d2aaadce452898e7534618f
Curtis Knudson <curtis@midgard.marketing>
2021-06-09 23:03:38 -0600

    Removed Material Ui as a dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Button from "@material-ui/core/Button/Button";
+import React from "react";
+
 export default function Home() {
   return (
     <div className="text-xl text-green-600 bg-blue-800">
       This is the commit that adds TailwindCSS
+      <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
     </div>
   );
 }
