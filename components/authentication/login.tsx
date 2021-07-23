8c2643978509a4736725c944f237bb2546546503
Curtis Knudson <curtis@midgard.marketing>
2021-07-22 23:24:10 -0600

    deleted bullshit middleware and passports

--- components/authentication/login.tsx
+++ components/authentication/login.tsx
@@ -2,22 +2,12 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import Router from "next/router";
 
-// Hooks
-import { useUser } from "hooks/useUser";
-
 const Login = () => {
-  const [t, { mutate }] = useUser();
   const [errorMsg, setErrorMsg] = useState("");
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
-
-  useEffect(() => {
-    // redirect to home if user is authenticated
-    if (t) Router.push("/");
-    // TODO: Create a toast notification that user is already signed in, and to sign out if they wish to use another account or create another account.
-  }, [t]);
 
   const handleChange = (e: any) => {
     const name = e.target.name;
@@ -36,9 +26,9 @@ const Login = () => {
     });
 
     if (res.status === 200) {
+      setErrorMsg("Awesome!");
       const userObj = await res.json();
-      // set user to useSWR state
-      mutate(userObj);
+      console.log(userObj);
     } else {
       setErrorMsg("Incorrect username or password. Try better!");
     }
