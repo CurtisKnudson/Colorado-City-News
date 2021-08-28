a79fff07ed3eaa23be929ed16085b00156d60d77
Curtis Knudson <curtis@midgard.marketing>
2021-08-27 19:45:42 -0600

    removed authentication

--- utils/saveToMongo.ts
+++ utils/saveToMongo.ts
@@ -1,5 +1,5 @@
 export const useSaveToMongo = async (data: any) => {
-  fetch("http://localhost:3000/api/article", {
+  fetch("http://localhost:3000/api/articles", {
     method: "POST",
     body: JSON.stringify({
       data: data,
