5d52bb87e66fb988ea140fea107653ec251350b4
Curtis Knudson <curtis@midgard.marketing>
2021-08-29 11:49:35 -0600

    refactored editor so it cannot be accessed without being signed in

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import useSideBarOpenContext from "@providers/sidebarContext";
 import { signIn, signOut, useSession } from "next-auth/client";
+import Link from "next/link";
 import * as React from "react";
 
 export const Sidebar = ({ children }: any) => {
@@ -47,6 +48,7 @@ export const Sidebar = ({ children }: any) => {
               >
                 Sign out
               </a>
+              <Link href="/write/editor">Editor</Link>
             </>
           )}
         </div>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
 export default NextAuth({
-  // https://next-auth.js.org/configuration/providers
-
   providers: [
     Providers.Email({
       server: {
@@ -21,18 +17,7 @@ export default NextAuth({
   ],
   database: process.env.MONGODB_URI,
   session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    // This option can be used with or without a database for users/accounts.
-    // Note: `jwt` is automatically set to `true` if no database is specified.
     jwt: true,
-
-    // Seconds - How long until an idle session expires and is no longer valid.
-    // maxAge: 30 * 24 * 60 * 60, // 30 days
-
-    // Seconds - Throttle how frequently to write to database to extend a session.
-    // Use it to limit write operations. Set to 0 to always update the database.
-    // Note: This option is ignored if using JSON Web Tokens
-    // updateAge: 24 * 60 * 60, // 24 hours
   },
   theme: "auto",
   debug: true,
