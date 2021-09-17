588d260f228148e674580f8a5fef46fec2a59e2d
Curtis Knudson <curtis@midgard.marketing>
2021-09-16 22:37:06 -0600

    Succesfully used context provider to manage state values for user profile

--- components/profile/setUserInfo.tsx
+++ components/profile/setUserInfo.tsx
@@ -1,14 +1,41 @@
+import { useUserProfileContext } from "@providers/profile";
+
 export const UserInfo = () => {
+  const [userProfileData, setUserProfileData] = useUserProfileContext();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserProfileData({
+      ...userProfileData,
+      [name]: value,
+      email: userProfileData.email,
+    });
+  };
+
+  console.log(userProfileData);
+
   return (
     <div className="flex flex-col mt-8">
       <label className="mt-8" htmlFor="name">
         Name:
       </label>
-      <input type="text" name="name" placeholder="Full Name" />
+      <input
+        type="text"
+        name="name"
+        value={userProfileData.name}
+        onChange={handleChange}
+        placeholder="Full Name"
+      />
       <label className="mt-8" htmlFor="email">
         Email:
       </label>
-      <input type="email" name="email" readOnly placeholder="Email Address" />
+      <input
+        type="email"
+        name="email"
+        readOnly
+        placeholder={userProfileData.email}
+        style={{ pointerEvents: "none" }}
+      />
       <div className="text-gray-500 italic text-xs mt-8">
         **If you want to comment, post or interact on this website you must have
         a name and profile picture
--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -4,11 +4,49 @@ import { Layout } from "@components/layout";
 import { useUserMediator } from "@mediator/providers/userMediatorProvider";
 import { Avatar, UserInfo } from "@components/profile";
 import { toast } from "react-toastify";
+import { useUserProfileContext } from "@providers/profile";
 
 const Profile = () => {
+  const mediator = useUserMediator();
   const { status } = useSession({
     required: true,
   });
+  const [userProfileData, setUserProfileData] = useUserProfileContext();
+
+  const handleSave = async () => {
+    if (
+      userProfileData.name &&
+      userProfileData.email &&
+      userProfileData.image
+    ) {
+      const userProfile = async () => {
+        let userProfile = await mediator
+          .completeUserProfile(userProfileData)
+          .then((res) => {
+            setUserProfileData(res);
+            return res;
+          })
+          .catch((err) => {
+            return err;
+          });
+        return userProfile;
+      };
+      toast
+        .promise(userProfile, {
+          pending: "Please wait...",
+          success: "Your account has been updated! 👌",
+          error: "There was an error 🤯. Contact admin@coloradocity.news ",
+        })
+        .then((res) => {
+          return res;
+        });
+      return;
+    }
+    toast.warning(
+      "Please make sure all of your data has been filled out. You must provide an image"
+    );
+    return;
+  };
 
   return (
     <Layout>
@@ -18,7 +56,10 @@ const Profile = () => {
         <>
           <Avatar />
           <UserInfo />
-          <div className="cursor-pointer border rounded w-14 center-all mx-auto mt-4">
+          <div
+            className="cursor-pointer border rounded w-14 center-all mx-auto mt-4"
+            onClick={handleSave}
+          >
             Save
           </div>
         </>
