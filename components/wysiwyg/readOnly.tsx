9459e5786984eba9fb42ade97029000f582b7898
Curtis Knudson <curtis@midgard.marketing>
2021-09-26 19:39:11 -0600

    Fixed typecasting for react slate

--- components/wysiwyg/readOnly.tsx
+++ components/wysiwyg/readOnly.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useMemo } from "react";
 import { createEditor, Descendant } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
 
 const ReadOnly = ({ content }: { content: any }) => {
   const [value, setValue] = useState<Descendant[]>(content);
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withReact(createEditor() as ReactEditor), []);
   return (
     <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
       <Editable readOnly placeholder="Enter some plain text..." />
