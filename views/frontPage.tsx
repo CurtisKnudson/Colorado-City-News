453b6250a5bf2eafe4d249243360b17d4bd176e7
Curtis Knudson <curtis@midgard.marketing>
2021-11-03 21:31:16 -0600

    Test for deployment

--- views/frontPage.tsx
+++ views/frontPage.tsx
@@ -3,6 +3,9 @@ import { ArticleCard, FeaturedArticleCard } from "@components/articleCard";
 import { ChipBar } from "@components/chipBar";
 
 const FrontPage = () => {
+  window.setTimeout(() => {
+    console.log("Do you see me? ");
+  }, 10000);
   return (
     <>
       <ChipBar />
