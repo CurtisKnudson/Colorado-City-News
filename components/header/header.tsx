9e7c54c503b2f4f58ca668fe9d5d2a121fd51434
Curtis Knudson <curtis@midgard.marketing>
2021-12-26 20:47:15 -0700

    removed ability to accidentally click the header button

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { SidebarButtonCollapser } from "@components/sidebarButtonCollapser";
 import useSideBarOpenContext from "@providers/sidebarContext";
-
 import Link from "next/link";
 import { Search } from "icons";
 
 export const Header = () => {
   const [navOpen, setNavOpen] = useSideBarOpenContext();
   return (
-    <>
-      <div className="mx-4 h-16 grid grid-cols-12">
-        <div className="col-span-1 flex items-center justify-center">
-          <SidebarButtonCollapser open={navOpen} setOpen={setNavOpen} />
-        </div>
-        <Link href="/" passHref>
-          <div className="font-chomsky col-span-7 text-xl flex items-center justify-center cursor-pointer select-none">
-            Colorado City News
-          </div>
-        </Link>
-
-        <div className="col-start-12 col-end-13 flex items-center justify-center">
-          <Search />
-        </div>
+    <div className="mx-4 h-16 grid grid-cols-12">
+      <div className="col-span-1 flex items-center justify-center">
+        <SidebarButtonCollapser open={navOpen} setOpen={setNavOpen} />
       </div>
-    </>
+
+      <div className="font-chomsky col-span-7 text-xl flex items-center justify-center  select-none">
+        <Link href="/" passHref>
+          <a
+            onClick={(e) => {
+              e.stopPropagation(), e.nativeEvent.stopImmediatePropagation();
+            }}
+            className="cursor-pointer"
+          >
+            Colorado City News
+          </a>
+        </Link>
+      </div>
+
+      <div className="col-start-12 col-end-13 flex items-center justify-center">
+        <Search />
+      </div>
+    </div>
   );
 };
