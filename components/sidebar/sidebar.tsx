5d76ca797999f65e31d72de692e767a290efb1e8
Curtis Knudson <curtis@midgard.marketing>
2021-12-28 20:07:01 -0700

    Added sidebar logic and infrastructure

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,24 +1,46 @@
 import * as React from "react";
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useUserProfileContext } from "@providers/profile";
 import { useSideBarOpenContext } from "@providers/sidebar/sidebarOpenProvider";
-// import { useState } from "react";
+import { MenuItem } from "./menuItem";
+import { useState } from "react";
+import Link from "next/link";
+
+export interface MenuItem {
+  label: string;
+  subNavItem?: {
+    label: string;
+    url?: string;
+  }[];
+  url?: string;
+}
 
 export const Sidebar = ({ children }: any) => {
   const { data: session } = useSession();
   const [userProfileData] = useUserProfileContext();
   const [navOpen, setNavOpen] = useSideBarOpenContext();
-  // const [selected, isSelected] = useState(false);
+  const [selected, setSelected] = useState<string>("Front Page");
 
-  const menuItemsArray: string[] = [
-    "Front Page",
-    "Social",
-    "Jobs",
-    "Housing",
-    "Classifieds",
-    "Obituaries",
-    "Sponsors",
+  const menuItemsArray: MenuItem[] = [
+    {
+      label: "Front Page",
+      url: "/",
+    },
+    { label: "Social", url: "/" },
+    { label: "Jobs", url: "/" },
+    { label: "Housing", url: "/" },
+    {
+      label: "Classifieds",
+      url: "/",
+      subNavItem: [
+        {
+          label: "For Sale",
+          url: "/",
+        },
+      ],
+    },
+    { label: "Obituaries", url: "/" },
+    { label: "Sponsors", url: "/" },
   ];
 
   const userProfileUrl = `/user/${session?.user.profileUrl}`;
@@ -30,13 +52,25 @@ export const Sidebar = ({ children }: any) => {
           navOpen ? "w-10/12" : "w-0"
         } fixed `}
       >
-        <div className="flex flex-col pl-4">
+        <div className="flex flex-col pl-4 mt-12">
           {!session && (
             <div>
-              <button onClick={() => signIn()}>Sign In</button>
               {menuItemsArray.map((item, index) => {
-                return <div key={index}>{item}</div>;
+                return (
+                  <MenuItem
+                    url={item.url ? item.url : "/"}
+                    label={item.label}
+                    setNavOpen={setNavOpen}
+                    selected={item.label === selected}
+                    key={index}
+                    onClick={() => setSelected(item.label)}
+                    subNavItem={item.subNavItem}
+                  />
+                );
               })}
+              <button className="mx-12 mt-16" onClick={() => signIn()}>
+                Sign In
+              </button>
             </div>
           )}
           {session && (
@@ -51,17 +85,9 @@ export const Sidebar = ({ children }: any) => {
                     : "Sign In"}
                 </strong>
               </span>
-              <MenuItem
-                url={userProfileUrl}
-                label="Profile"
-                setNavOpen={setNavOpen}
-              />
+              <Link href={userProfileUrl}>Profile</Link>
               {userProfileData.isWriter ? (
-                <MenuItem
-                  url="/write/editor"
-                  label="Editor"
-                  setNavOpen={setNavOpen}
-                />
+                <Link href="/write/editor">Editor</Link>
               ) : null}
 
               <hr className="mt-8" />
@@ -72,22 +98,6 @@ export const Sidebar = ({ children }: any) => {
         </div>
       </div>
       <div className="mx-4"> {children}</div>
-    </div>
-  );
-};
-
-const MenuItem = ({
-  url,
-  label,
-  setNavOpen,
-}: {
-  url: string;
-  label: string;
-  setNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
-  return (
-    <div className="mt-4" onClick={() => setNavOpen(false)}>
-      <Link href={url}>{label}</Link>
     </div>
   );
 };
